71036f0b4a3bba9564f1505d3bb7b67f54cccc4f
piasahmed1 <ahmedpias229@gmail.com>
2023-10-21 20:44:08 +0600

    fix some error and problem

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-
-// middleware
+// middleware added
 app.use(cors());
 app.use(express.json());
 
@@ -44,7 +43,6 @@ async function run() {
       res.send(result);
     })
 
-
     app.get('/product/:id', async(req, res) => {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}
@@ -77,6 +75,7 @@ async function run() {
         }
       }
 
+
       const result = await productCollection.updateOne(filter, Product, options)
       res.send(result);
 
@@ -95,7 +94,6 @@ run().catch(console.dir);
 app.get('/', (req, res) =>{
   res.send('Assignment making Server is Running')
 })
-
 
 app.listen(port, () => {
   console.log(`Assignment Server is running on port ${port}`)
