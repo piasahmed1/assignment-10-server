439c0dd5e9b317562d265bd454f87bfb2fb7b70a
piasahmed1 <ahmedpias229@gmail.com>
2023-10-21 17:07:21 +0600

    backend almost complete

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 console.log(uri);
 
-
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -102,5 +101,3 @@ app.get('/', (req, res) =>{
 app.listen(port, () => {
   console.log(`Assignment Server is running on port ${port}`)
 })
-
-
